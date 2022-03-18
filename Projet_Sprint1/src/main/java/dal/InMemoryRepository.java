5e1b45a69c5daca37647b6c5d8879fdf4c3b8610
Aspirot <81372169+Aspirot@users.noreply.github.com>
2022-03-18 16:04:16 -0400

    VoteDAO

--- Projet_Sprint1/src/main/java/dal/InMemoryRepository.java
+++ Projet_Sprint1/src/main/java/dal/InMemoryRepository.java
@@ -1,4 +1,48 @@
 package dal;
 
+import bll.model.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class InMemoryRepository {
+    private List<Vote> votes;
+    private List<Candidate> candidates;
+    private List<Elector> electors;
+    private List<Ballot> ballots;
+    private List<Post> posts;
+    private List<Forum> forums;
+
+    public InMemoryRepository(){
+        this.ballots = new ArrayList<>();
+        this.candidates = new ArrayList<>();
+        this.electors = new ArrayList<>();
+        this.forums = new ArrayList<>();
+        this.posts = new ArrayList<>();
+        this.votes = new ArrayList<>();
+    }
+
+    public List<Vote> getVotes() {
+        return votes;
+    }
+
+    public List<Candidate> getCandidates() {
+        return candidates;
+    }
+
+    public List<Elector> getElectors() {
+        return electors;
+    }
+
+    public List<Ballot> getBallots() {
+        return ballots;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public List<Forum> getForums() {
+        return forums;
+    }
 }
--- Projet_Sprint1/src/main/java/dal/VoteDAO.java
+++ Projet_Sprint1/src/main/java/dal/VoteDAO.java
@@ -3,33 +3,33 @@ package dal;
 import bll.model.Vote;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 public class VoteDAO implements IVoteDAO{
     private List<Vote> votes;
 
-    public VoteDAO() {
-        votes = new ArrayList<>();
+    public VoteDAO(InMemoryRepository memoryRepository) {
+        this.votes = new ArrayList<>();
+        memoryRepository.getVotes().stream().forEach(v -> saveVote(v));
     }
 
     @Override
     public void saveVote(Vote vote) {
-        votes.add(vote);
+        this.votes.add(vote);
     }
 
     @Override
     public List<Vote> getAllVotes() {
-        return null;
+        return this.votes;
     }
 
     @Override
     public void changeVote(int voteId, int newPollSubjectId) {
-
+        this.votes.stream().filter(v -> v.getId()==voteId).findFirst().get().setPollSubjectId(newPollSubjectId);
     }
 
     @Override
     public void deleteVote(int voteId) {
-
+        this.votes.remove(this.votes.stream().filter(v -> v.getId()==voteId).findFirst());
     }
 }
