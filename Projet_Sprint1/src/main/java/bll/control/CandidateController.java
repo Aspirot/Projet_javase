f7e2f79dfb79ee1d581e98fc2b79a13b722fd104
Guilbault2063074 <81373503+Guilbault2063074@users.noreply.github.com>
2022-03-18 18:32:43 -0400

    Update CandidateController.java

--- Projet_Sprint1/src/main/java/bll/control/CandidateController.java
+++ Projet_Sprint1/src/main/java/bll/control/CandidateController.java
@@ -1,10 +1,18 @@
 package bll.control;
 
+import bll.model.Ballot;
+import bll.model.Candidate;
+import dal.BallotDAO;
 import dal.CandidateDAO;
 import dal.ICandidateDAO;
 
 public class CandidateController {
     private ICandidateDAO candidateDAO;
+
+    public void addCanditateToElection(int canditateId, int pollId, BallotController ballotController)
+    {
+        BallotController.getBallotDAO().fetchBallotById(pollId).get().add(candidateDAO.getAllCanditates().stream().filter(c -> c.getId()==canditateId).findFirst());
+    }
 
     public CandidateController(CandidateDAO candidateDAO){
         this.candidateDAO = candidateDAO;
