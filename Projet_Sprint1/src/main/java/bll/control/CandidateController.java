462953bdb93cd906663bf715fea409f1564dd45f
Aspirot <81372169+Aspirot@users.noreply.github.com>
2022-03-18 18:49:51 -0400

    findNumberOfInFavorByCandidate method

--- Projet_Sprint1/src/main/java/bll/control/CandidateController.java
+++ Projet_Sprint1/src/main/java/bll/control/CandidateController.java
@@ -9,12 +9,16 @@ import dal.ICandidateDAO;
 public class CandidateController {
     private ICandidateDAO candidateDAO;
 
+    public CandidateController(CandidateDAO candidateDAO){
+        this.candidateDAO = candidateDAO;
+    }
+
     public void addCandidateToElection(int candidateId, int pollId, BallotController ballotController)
     {
         ballotController.getBallotDAO().fetchBallotById(pollId).get().addCandidate(candidateDAO.getAllCanditates().stream().filter(c -> c.getId()==candidateId).findFirst().get());
     }
 
-    public CandidateController(CandidateDAO candidateDAO){
-        this.candidateDAO = candidateDAO;
+    public int findNumberOfInFavorByCandidate(int candidateId, VoteController voteController){
+        return voteController.getVoteDAO().getAllVotes().stream().filter(v -> v.getPollSubjectId()==candidateId).toList().size();
     }
 }
