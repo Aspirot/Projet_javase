ad20a052b85b9787f98c935f269eb70f5c54c328
Aspirot <81372169+Aspirot@users.noreply.github.com>
2022-03-18 15:22:37 -0400

    implements IVoteDAO

--- Projet_Sprint1/src/main/java/dal/VoteDAO.java
+++ Projet_Sprint1/src/main/java/dal/VoteDAO.java
@@ -1,4 +1,28 @@
 package dal;
 
-public class VoteDAO {
+import bll.model.Vote;
+
+import java.util.Date;
+import java.util.List;
+
+public class VoteDAO implements IVoteDAO{
+    @Override
+    public void createVote(int pollId, int pollSubjectId, int electorId, Date when, int rank) {
+        
+    }
+
+    @Override
+    public List<Vote> getAllVotes() {
+        return null;
+    }
+
+    @Override
+    public void changeVote(int voteId, int newPollSubjectId) {
+
+    }
+
+    @Override
+    public void deleteVote(int voteId) {
+
+    }
 }
