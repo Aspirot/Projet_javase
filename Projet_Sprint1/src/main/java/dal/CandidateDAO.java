7e1bd4e489c8e7832c8c8b8e82051431c58c7fa5
Guilbault2063074 <81373503+Guilbault2063074@users.noreply.github.com>
2022-03-18 16:45:06 -0400

    cdao

--- Projet_Sprint1/src/main/java/dal/CandidateDAO.java
+++ Projet_Sprint1/src/main/java/dal/CandidateDAO.java
@@ -1,4 +1,30 @@
 package dal;
 
+import bll.model.Candidate;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CandidateDAO implements ICandidateDAO{
+    private List<Candidate> candidates;
+
+    public CandidateDAO(InMemoryRepository memoryRepository) {
+        this.candidates = new ArrayList<>();
+        memoryRepository.getCandidates().stream().forEach(c -> savecandidate(c));
+    }
+
+    @Override
+    public void savecandidate(Candidate candidate) {
+        this.candidates.add(candidate);
+    }
+
+    @Override
+    public List<Candidate> getAllCanditates() {
+        return this.candidates;
+    }
+
+    @Override
+    public void deleteCanditate(int candidateId) {
+        this.candidates.remove(this.candidates.stream().filter(c -> c.getId()==candidateId).findFirst());
+    }
 }
--- Projet_Sprint1/src/main/java/dal/ICandidateDAO.java
+++ Projet_Sprint1/src/main/java/dal/ICandidateDAO.java
@@ -1,4 +1,12 @@
 package dal;
 
+import bll.model.Candidate;
+
+import java.util.List;
+
 public interface ICandidateDAO {
+    void savecandidate(Candidate candidate);
+    List<Candidate> getAllCanditates();
+    void deleteCanditate(int candidateId);
+
 }
